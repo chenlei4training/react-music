b12601eb822fb5f7e2ba3e89e438a29b59e6887d
chen_lei <2624304501@qq.com>
2019-07-18 16:43:27 +0800

    add interceptor

--- src/util/ajax.js
+++ src/util/ajax.js
@@ -2,7 +2,23 @@ import axios from 'axios'
 
 //instance 实例
 const axiosIns = axios.create({
-    baseURL:'http://localhost:2019/'
+    baseURL: 'http://localhost:2019/'
 })
+
+// Add a request interceptor
+axiosIns.interceptors.request.use(function (config) {
+     const token = localStorage.getItem('token')
+    if (token) {
+        let newHeaders = { ...config.headers, Authorization: `Bearer ${token}` }
+        let newConfig = { ...config, headers: { ...newHeaders } }
+
+        return newConfig;
+    } else {
+        return config
+    }
+}, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+});
 
 export default axiosIns
