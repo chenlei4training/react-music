3af70bcf5ae49ae5493a92ba5035fecb21258851
chen_lei <2624304501@qq.com>
2019-09-11 19:27:37 +0800

    user validate

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { withRouter, NavLink,Link} from 'react-router-dom'
+import { withRouter, NavLink, Link } from 'react-router-dom'
 import ajax from '../../util/ajax'
 class Login extends Component {
     render() {
@@ -8,7 +8,7 @@ class Login extends Component {
             <div className="login-container">
                 <NavLink to='/login/signin' activeClassName='selected'>登陆</NavLink>
                 <NavLink to='/login/register' activeClassName='selected'>注册</NavLink>
-                
+
                 <Link to="/home" className="float-right">主页</Link>
                 {this.renderOption(this.props)}
 
@@ -24,8 +24,11 @@ class Login extends Component {
         })
             .then((result) => {
                 console.log('server result', result)
-                if (result.data.token) {
+                if (result.data.code ===1 && result.data.token) {
                     localStorage.setItem('token', result.data.token)
+                    this.props.history.push('/home')
+                }else{
+                    alert(result.data.msg)
                 }
             })
     }
@@ -37,14 +40,25 @@ class Login extends Component {
     }
 
     register = () => {
-        ajax.post('/user/register',{
-            user:this.refs.user.value,
-            password:this.refs.password.value
-        }).then((result)=>{
-            alert(JSON.stringify(result))
+        if(this.refs.password.value !== this.refs.re_password.value){
+            alert('输入的两次密码不一致')
+            return
+        }
+
+        ajax.post('/user/register', {
+            user: this.refs.user.value,
+            password: this.refs.password.value
+        }).then((result) => {
+            // alert(JSON.stringify(result))
+            if(result.data.code ===1 && result.data.token){
+                localStorage.setItem('token',result.data.token)
+                this.props.history.push('/home')
+            }else{
+                alert(result.data.msg)
+            }
         })
     }
-    renderOption = (props)=> {
+    renderOption = (props) => {
         if (props.match.params.id === 'signin') { //signin
             return (<div className="login-content">
                 <div>登录</div>
@@ -52,7 +66,7 @@ class Login extends Component {
                 <div><input type="password" ref="password" placeholder="你的密码" /></div>
 
                 <div><input type="button" onClick={this.jumpToHome} defaultValue="登录" /></div>
-  
+
             </div>
             )
         } else { //register
@@ -60,10 +74,10 @@ class Login extends Component {
                 <div>注册</div>
                 <div><input type="text" ref="user" placeholder="你的用户名" /></div>
                 <div><input type="password" ref="password" placeholder="你的密码" /></div>
-                <div><input type="password" ref="re-password" placeholder="再次输入你的密码" /></div>
+                <div><input type="password" ref="re_password" placeholder="再次输入你的密码" /></div>
 
                 <div><input type="button" onClick={this.register} defaultValue="注册" /></div>
-     
+
             </div>
             )
         }
