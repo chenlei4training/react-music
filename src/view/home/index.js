9252b099374cada8747c9383849d2218ab2688f1
chen_lei <2624304501@qq.com>
2019-09-13 18:32:21 +0800

    add home with style

--- src/view/home/index.js
+++ src/view/home/index.js
@@ -2,16 +2,55 @@ import React, { Component } from "react"
 
 import {withRouter} from 'react-router-dom'
 
-
+import "../../looking/scss/home.scss"
 //主页组件
 class Home extends Component {
     //html onclick="fun(event)"
     render() {
-        return <div>
-            这是我的主页
-              <input type="button" onClick={this.jumpToLogin} value="跳到登录页面">
-            </input>
-        </div>
+        return (
+            <div className="home">
+                <div className="g-topbar">
+                    <div className="m-top">
+                        <div className="wrap f-cb">
+                            <div className="logo"> xxxx </div>
+
+                            {/* 菜单导航 */}
+                            <ul className="m-nav">
+                                <li> <a href="#">发现音乐</a></li>
+                                <li> <a href="#">我的音乐</a></li>
+                                <li> <a href="#">朋友</a></li>
+                                <li> <a href="#">商城</a></li>
+                                <li> <a href="#">音乐人</a></li>
+                                <li> <a href="#">下载客户端</a></li>
+                                <li> <sup className="hot">&nbsp;</sup></li>
+                            </ul>
+                            {/* 浮动到右边的类容 */}
+                            {/* 登录 */}
+                            <div className="m-tophead">
+                                <a href="#">登录</a>
+                            </div>
+
+                            <a href="#" className="m-topvd">创作中心</a>
+
+                            <div class="srchbg">
+                                <i className="icon">&nbsp;</i> 
+                                <input type="text" placeholder="歌曲名称"></input>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className="conetent"> 这里是主要内容</div>
+            </div>
+            
+        )
+        // return <div>
+        //     这是我的主页
+        //       <input type="button" onClick={this.jumpToLogin} value="跳到登录页面">
+        //     </input>
+        // </div>
     }
 
     jumpToLogin = () => {
