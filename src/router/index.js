b14878b9ccf945fa32df818353d5c2b919a3ef53
chen_lei <2624304501@qq.com>
2019-09-15 12:20:57 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {HashRouter,BrowserRouter,StaticRouter,MemoryRouter,Route,Switch,Redirect} from 'react-router-dom'
+import { HashRouter, BrowserRouter, StaticRouter, MemoryRouter, Route, Switch, Redirect } from 'react-router-dom'
 //StaticRouter 服务器端渲染才使用
 //MemoryRouter 一般为React Native项目里使用 
 
@@ -10,16 +10,20 @@ import Login from '../view/login'
 
 import NoPage404 from '../view/pageNoFound404'
 
-function AppRouter(){
+function AppRouter() {
     return <BrowserRouter>
-                <Switch>
-                    <Route exact path="/" render={() => {return <Redirect to="/home"/>}}/>
-                    <Route path="/home" component={Home}/> 
-                    <Route exact path="/login/:id" component={Login}/>
-                    {/* 垫底的路由匹配 需要Switch配合 */}
-                    <Route  component= {NoPage404}/> 
-                </Switch>
-            </BrowserRouter>
+        <Switch>
+            {/* <Route exact path="/" render={() => {return <Redirect to="/home"/>}}/> */}
+            <Route exact path="/" component={Home} />
+
+            <Route exact path="/:main_nav/" component={Home} />
+            <Route exact path="/:main_nav/:sub_nav" component={Home} />
+            {/* <Route path="/home/:top/:sub" component={Home}/>  */}
+            <Route path="/login/:id" component={Login} />
+            {/* 垫底的路由匹配 需要Switch配合 */}
+            <Route component={NoPage404} />
+        </Switch>
+    </BrowserRouter>
 }
 
 export default AppRouter
